4eee92a6cb92c288cf3a513d6e20764330785e49
Patrick Timothee <ptimothee@junipercommerce.com>
2022-06-07 17:04:06 -0400

    updated sample project

---
+++ Sample.AzureCognitiveSearch.DI/Contexts/AzureSearchContext.cs
@@ -0,0 +1,21 @@
+﻿using Sample.AzureCognitiveSearch.DI.Contexts.Models;
+
+/// <summary>
+/// Azure Context using contructor injection 
+/// </summary>
+public class AzureSearchContext
+{
+    /// <summary>
+    /// Azure Model that you can query on
+    /// </summary>
+    public IQueryable<AzureModel> QueryModel { get; private set; }
+
+    /// <summary>
+    /// ctor with queryable model
+    /// </summary>
+    /// <param name="indexName"></param>
+    public AzureSearchContext(IQueryable<AzureModel> query)
+	{
+		QueryModel = query;
+	}
+}
--- Sample.AzureCognitiveSearch.DI/Controllers/SearchController.cs
+++ Sample.AzureCognitiveSearch.DI/Controllers/SearchController.cs
@@ -18,14 +18,21 @@ public class SearchController : ControllerBase
     private AzureContextAttributes _contextAttributes;
 
     /// <summary>
+    /// Context using constructor injection
+    /// </summary>
+    private AzureSearchContext _searchContext;
+
+    /// <summary>
     /// Constructor
     /// </summary>
     /// <param name="contextFluentApi">instance of context using fluent api</param>
     /// <param name="contextAttributes">instance of context using attributes</param>
+    /// <param name="searchContext">instance of context defined at application level</param>
     /// <exception cref="ArgumentNullException"></exception>
-    public SearchController(AzureContextFluentApi contextFluentApi, AzureContextAttributes contextAttributes)
+    public SearchController(AzureContextFluentApi contextFluentApi, AzureContextAttributes contextAttributes, AzureSearchContext searchContext)
     {
         _contextFluentApi = contextFluentApi ?? throw new ArgumentNullException(nameof(contextFluentApi));
+        _contextAttributes = contextAttributes ?? throw new ArgumentNullException(nameof(contextAttributes));
         _contextAttributes = contextAttributes ?? throw new ArgumentNullException(nameof(contextAttributes));
     }
 }
